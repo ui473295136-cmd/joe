2aff534fd0954ae6b8e7388a2c661273ae230f0c
ui473295136-cmd <ui473295136@163.com>
2026-09-14 19:17:55 +0800

    Bump lightweight cache after GPS tuning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chuanxi-2026-v19';
+const CACHE='chuanxi-2026-v20';
 const CORE=['./','./index.html','./app-v4.html','./app-v4.css','./app-v4.js','./geo-throttle.js','./avatars.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
